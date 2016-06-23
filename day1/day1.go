21acbe34b9a410da29b6852ac326d2fdf9bdd129
Dean Oren <d.oren@initial.uk.com>
2016-06-23 17:16:55 +0100

    day 1 completed

--- day1/day1.go
+++ day1/day1.go
@@ -3,14 +3,22 @@ package main
 /**
   '(' means up, while ')' menas down
   parse a string of brackets in order to get the final floor (starting from 0)
+  The function will return the final floor and the first index in which the
+  player reached the basement level (-1)
 */
-func calculateFloor(input string) int {
-  up, down := 0, 0
+func calculateFloor(input string) (int, int) {
+  up, down, basementIndex := 0, 0, 0
   for _, r := range input {
       switch r {
         case '(': up++
         case ')': down++
       }
+
+      /* check if the floor is -1 and that the basement index
+      hasn't been updated yet */
+      if -1 == up-down && 0 == basementIndex {
+        basementIndex = up+down;
+      }
   }
-  return up-down;
+  return up-down, basementIndex;
 }
