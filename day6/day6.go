084fde74bc677c11490b74bd7a36659e134981c5
Dean Oren <d.oren@initial.uk.com>
2016-07-13 12:43:24 +0100

    day 6 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -5,9 +5,19 @@ import (
 	"strings"
 )
 
+const stateOn = "on"
+const stateOff = "off"
+const stateToggle = "toggle"
+
 // Grid system
 type Grid struct {
 	Map      map[int]map[int]bool
+	TurnedOn int
+}
+
+// NewGrid system
+type NewGrid struct {
+	Map      map[int]map[int]int
 	TurnedOn int
 }
 
@@ -28,17 +38,17 @@ func (grid *Grid) ChangeLigtState(state string, from string, to string) *Grid {
 			}
 
 			switch state {
-			case "on":
+			case stateOn:
 				if grid.Map[i][j] == false {
 					grid.Map[i][j] = true
 					grid.TurnedOn++
 				}
-			case "off":
+			case stateOff:
 				if grid.Map[i][j] {
 					grid.Map[i][j] = false
 					grid.TurnedOn--
 				}
-			case "toggle":
+			case stateToggle:
 				if grid.Map[i][j] {
 					grid.TurnedOn--
 					grid.Map[i][j] = false
@@ -46,6 +56,43 @@ func (grid *Grid) ChangeLigtState(state string, from string, to string) *Grid {
 					grid.TurnedOn++
 					grid.Map[i][j] = true
 				}
+			}
+		}
+	}
+
+	// fmt.Printf("Action: %s From %s To %s, current: %d\n", state, from, to, grid.TurnedOn)
+
+	return grid
+}
+
+// ChangeNewLigtState changes the state of the lights according to the coordinates
+func (grid *NewGrid) ChangeNewLigtState(state string, from string, to string) *NewGrid {
+	fromSplit := strings.Split(from, ",")
+	toSplit := strings.Split(to, ",")
+
+	if grid.Map == nil {
+		grid.Map = make(map[int]map[int]int)
+	}
+
+	for i, n := strToInt(fromSplit[0]), strToInt(toSplit[0]); i <= n; i++ {
+		for j, m := strToInt(fromSplit[1]), strToInt(toSplit[1]); j <= m; j++ {
+			if grid.Map[i] == nil {
+				grid.Map[i] = make(map[int]int)
+				grid.Map[i][j] = 0
+			}
+
+			switch state {
+			case stateOn:
+				grid.TurnedOn++
+				grid.Map[i][j]++
+			case stateOff:
+				if grid.Map[i][j] > 0 {
+					grid.Map[i][j]--
+					grid.TurnedOn--
+				}
+			case stateToggle:
+				grid.TurnedOn += 2
+				grid.Map[i][j] += 2
 			}
 		}
 	}
@@ -66,8 +113,26 @@ func (grid *Grid) ParseInstructions(instructions string) *Grid {
 		switch inst[0] {
 		case "turn":
 			grid.ChangeLigtState(inst[1], inst[2], inst[4])
-		case "toggle":
+		case stateToggle:
 			grid.ChangeLigtState(inst[0], inst[1], inst[3])
+		}
+	}
+	return grid
+}
+
+// ParseNewInstructions Parses a given string instruction
+func (grid *NewGrid) ParseNewInstructions(instructions string) *NewGrid {
+	rows := strings.Split(instructions, "\n")
+
+	// check each row
+	for _, row := range rows {
+		row = strings.Trim(row, "	")
+		inst := strings.Split(row, " ")
+		switch inst[0] {
+		case "turn":
+			grid.ChangeNewLigtState(inst[1], inst[2], inst[4])
+		case stateToggle:
+			grid.ChangeNewLigtState(inst[0], inst[1], inst[3])
 		}
 	}
 	return grid
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -13,10 +13,28 @@ var testPart1 = struct {
 	expected: 998996,
 }
 
+var testPart2 = struct {
+	strings  string
+	expected int
+}{
+
+	strings: `turn on 0,0 through 0,0
+	toggle 0,0 through 999,999`,
+	expected: 2000001,
+}
+
 func TestStringsPart1(t *testing.T) {
 	grid := Grid{}
 	on := grid.ParseInstructions(testPart1.strings).TurnedOn
 	if testPart1.expected != on {
 		t.Errorf("Number of lights on (%d) doesn't match what was expected (%d)", on, testPart1.expected)
+	}
+}
+
+func TestStringsPart2(t *testing.T) {
+	grid := NewGrid{}
+	on := grid.ParseNewInstructions(testPart2.strings).TurnedOn
+	if testPart2.expected != on {
+		t.Errorf("Number of lights on (%d) doesn't match what was expected (%d)", on, testPart2.expected)
 	}
 }
